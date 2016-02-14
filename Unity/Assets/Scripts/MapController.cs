2f1d3499b26a45dac15aa7f5701664f6addbeb18
Travis <travis.cox@unimelb.edu.au>
2016-02-14 14:58:56 +1100

    Changed classes to track via dictionaries of nodes and ways

--- Unity/Assets/Scripts/MapController.cs
+++ Unity/Assets/Scripts/MapController.cs
@@ -18,6 +18,9 @@ public class MapController : MonoBehaviour {
 	private float _minLon = float.MaxValue;
 	private float _maxLat = float.MaxValue;
 	private float _maxLon = float.MaxValue;
+	
+	private IDictionary<double, IList<double>> _wayDictionary;
+	private IDictionary<double, float[]> _nodeDictionary;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +29,9 @@ public class MapController : MonoBehaviour {
 		
 		_nodes = _mapXML.SelectNodes ("//node");
 		_ways = _mapXML.SelectNodes ("//way");
+
+		_wayDictionary = new Dictionary<double, IList<double>> ();
+		_nodeDictionary = new Dictionary<double, float[]> ();
 
 		XmlNodeList _bounds = _mapXML.SelectNodes ("//bounds");
 
@@ -41,6 +47,9 @@ public class MapController : MonoBehaviour {
 		foreach (XmlNode n in _nodes) {
 			float x = float.Parse (n.Attributes.GetNamedItem ("lat").Value);
 			float y = float.Parse (n.Attributes.GetNamedItem ("lon").Value);
+
+			_nodeDictionary.Add (double.Parse (n.Attributes.GetNamedItem ("id").Value), new float[] {x, y});
+
 			if (_minLat > x || _minLat == float.MaxValue) {
 				_minLat = x;
 			}
@@ -67,7 +76,6 @@ public class MapController : MonoBehaviour {
 		}
 
 		foreach (XmlNode w in _ways) {
-			XmlNodeList nd = w.SelectNodes("//nd");
 			bool isHighway = false;
 			//First check if this way has a highway tag.
 			foreach (XmlNode t in w.ChildNodes) {
@@ -76,34 +84,44 @@ public class MapController : MonoBehaviour {
 						isHighway = true;
 					}
 				}
+			}
 			
-				if (isHighway) {
-					XmlNode from = null;
-					XmlNode to = null;
-					foreach (XmlNode wayNode in nd) {
-
-							to = getNodeByID (double.Parse (wayNode.Attributes.GetNamedItem ("ref").Value), _nodes);
-
-							if (to == null) {
-								continue;
-							}
-							if (from == null) {
-								from = to;
-								continue;
-							}
-
-							float fromLat = MapMetaInformation.Instance.MapLatValue (float.Parse (from.Attributes.GetNamedItem ("lat").Value));
-							float fromLon = MapMetaInformation.Instance.MapLonValue (float.Parse (from.Attributes.GetNamedItem ("lon").Value));
-
-							float toLat = MapMetaInformation.Instance.MapLatValue (float.Parse (to.Attributes.GetNamedItem ("lat").Value));
-							float toLon = MapMetaInformation.Instance.MapLonValue (float.Parse (to.Attributes.GetNamedItem ("lon").Value));
-
-							Debug.DrawLine (new Vector3 (fromLat, 0, fromLon), new Vector3 (toLat, 0, toLon));
-					}
+			if (isHighway) {
+				//If it is a highway then get all the nd references into a list then add the way id and the node refs into the wayDictionary
+				double wayId = double.Parse (w.Attributes.GetNamedItem ("id").Value);
+				IList<double> wayNodes = new List<double> ();
+				//If is highway add it to the way Dictionary.
+				XmlNodeList nd = w.SelectNodes ("nd");
+				foreach (XmlNode wayNode in nd) {
+					wayNodes.Add (double.Parse (wayNode.Attributes.GetNamedItem ("ref").Value));
 				}
+				_wayDictionary.Add (wayId, wayNodes);
 			}
 		}
 	}
+
+//					XmlNode from = null;
+//					XmlNode to = null;
+//					foreach (XmlNode wayNode in nd) {
+//						if (wayNode.Name == "nd") {
+//							to = getNodeByID (double.Parse (wayNode.Attributes.GetNamedItem ("ref").Value), _nodes);
+//
+//							if (to == null) {
+//								continue;
+//							}
+//							if (from == null) {
+//								from = to;
+//								continue;
+//							}
+//
+//							float fromLat = MapMetaInformation.Instance.MapLatValue (float.Parse (from.Attributes.GetNamedItem ("lat").Value));
+//							float fromLon = MapMetaInformation.Instance.MapLonValue (float.Parse (from.Attributes.GetNamedItem ("lon").Value));
+//
+//							float toLat = MapMetaInformation.Instance.MapLatValue (float.Parse (to.Attributes.GetNamedItem ("lat").Value));
+//							float toLon = MapMetaInformation.Instance.MapLonValue (float.Parse (to.Attributes.GetNamedItem ("lon").Value));
+//
+//							//Debug.DrawLine (new Vector3 (fromLat, 0, fromLon), new Vector3 (toLat, 0, toLon));
+
 
 
 //		while (_mapXML.Read()) {
