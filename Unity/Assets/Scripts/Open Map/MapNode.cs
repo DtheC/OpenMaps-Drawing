13fce5846c7224fbd125b7fb24e02c4767a3d4b5
Travis <travis.cox@unimelb.edu.au>
2016-02-21 10:06:36 +1100

    Begun adding class system for nodes/ways instead of Dictionaries

---
+++ Unity/Assets/Scripts/Open Map/MapNode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapNode {
+	
+	private double _id {get; set;}
+	private IDictionary<string, IList<string>> _tags {get; set;}
+	private float _lat {get; set;}
+	private float _lon {get; set;}
+
+	private Vector3 _latInUnits {get; set;}
+	private Vector3 _lonInUnits {get; set;}
+	
+	public MapNode(double id, float lat, float lon){
+		_id = id;
+		_lat = lat;
+		_lon = lon;
+	}
+
+}
