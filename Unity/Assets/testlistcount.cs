f182a3a8f73dda266d756dcfd62e980052a961ab
Travis Cox <travis@denouncetheclock.com>
2017-02-02 09:26:31 +1100

    Urban ReNeural Version

---
+++ Unity/Assets/testlistcount.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class testlistcount : MonoBehaviour {
+    public IList<float> ConnectedNodes { get; set; }
+    // Use this for initialization
+    void Start () {
+        ConnectedNodes = new List<float>();
+        Debug.Log(ConnectedNodes.Count);
+        ConnectedNodes.Add(4f);
+        Debug.Log(ConnectedNodes.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
